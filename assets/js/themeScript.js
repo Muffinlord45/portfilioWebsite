435a3630e503305b0b1474a1c55ad8474abd482c
VioletTides <ross34anderson@gmail.com>
2022-09-26 11:57:47 -0400

    theme sun/moon icons

--- assets/js/themeScript.js
+++ assets/js/themeScript.js
@@ -1,4 +1,5 @@
 document.querySelector('.theme-toggle-button').addEventListener('click', () => {
     document.body.classList.toggle('dark')
-    classList.toggle(visually-hidden)
+    document.getElementById("sun").classList.toggle('visually-hidden')
+    document.getElementById("moon").classList.toggle('visually-hidden')
 })
