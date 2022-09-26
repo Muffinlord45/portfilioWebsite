ac657873cd01f5c8e7aa169e5fc28d05303c301a
VioletTides <ross34anderson@gmail.com>
2022-09-26 13:08:56 -0400

    dark/light me background

--- assets/js/themeScript.js
+++ assets/js/themeScript.js
@@ -1,5 +1,9 @@
 document.querySelector('.theme-toggle-button').addEventListener('click', () => {
     document.body.classList.toggle('dark')
+    
     document.getElementById("sun").classList.toggle('visually-hidden')
     document.getElementById("moon").classList.toggle('visually-hidden')
+    
+    document.getElementById("meDark").classList.toggle('visually-hidden')
+    document.getElementById("meLight").classList.toggle('visually-hidden')
 })
