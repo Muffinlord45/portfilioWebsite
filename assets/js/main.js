4098193c1da9077dd4dd9dcc951847fa4afd1b62
VioletTides <ross34anderson@gmail.com>
2022-09-28 00:06:45 -0400

    fixed dark/light mode frfr this time

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,24 @@
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    document.body.classList.toggle('dark')
+    localStorage.setItem('theme', 'dark');
+     console.log("dark")
+       document.getElementById("sun").classList.add('visually-hidden');
+       document.getElementById("moon").classList.remove('visually-hidden');
+       document.getElementById("meLight").classList.add('visually-hidden');
+       document.getElementById("meDark").classList.remove('visually-hidden');
+    
+    document.body.classList.add('dark');
+    document.body.classList.remove('light');
 }
-
-document.querySelector('.theme-toggle-button').addEventListener('click', () => {
+else{
+    localStorage.setItem('theme', 'light');
+    console.log("light")
+       document.getElementById("sun").classList.remove('visually-hidden');
+       document.getElementById("moon").classList.add('visually-hidden');
+       document.getElementById("meLight").classList.remove('visually-hidden');
+       document.getElementById("meDark").classList.add('visually-hidden');
     
-    if(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches){   
-        localStorage.setItem('theme', 'dark');
-        document.body.classList.toggle('dark');
-        document.body.classList.toggle('light');
-    }
-    else{
-        localStorage.setItem('theme', 'light');
-        document.body.classList.toggle('dark');
-        document.body.classList.toggle('light');
-    }
-    console.log(localStorage);
-    document.getElementById("sun").classList.toggle('visually-hidden')
-    document.getElementById("moon").classList.toggle('visually-hidden')
-    
-    document.getElementById("meDark").classList.toggle('visually-hidden')
-    document.getElementById("meLight").classList.toggle('visually-hidden')
-    
-})
+    document.body.classList.remove('dark');
+    document.body.classList.add('light');
+}
 
 
