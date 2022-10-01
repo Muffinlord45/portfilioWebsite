0ef510a1e50853465d642da7a71e81dcf676997c
VioletTides <ross34anderson@gmail.com>
2022-09-30 22:26:16 -0400

    pagewide darkmode

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,35 @@
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    switch_to_dark();
+}
+else{
+    switch_to_light();
+}
+
+
+function switch_to_dark(){
     localStorage.setItem('theme', 'dark');
-     console.log("dark")
-       document.getElementById("sun").classList.add('visually-hidden');
+     console.log("dark");
+       if(window.location.href == "https://violettides.github.io/"){
+           document.getElementById("sun").classList.add('visually-hidden');
        document.getElementById("moon").classList.remove('visually-hidden');
        document.getElementById("meLight").classList.add('visually-hidden');
        document.getElementById("meDark").classList.remove('visually-hidden');
+       }
     
     document.body.classList.add('dark');
     document.body.classList.remove('light');
 }
-else{
+
+function switch_to_light(){
     localStorage.setItem('theme', 'light');
-    console.log("light")
+    console.log("light");
+    if(window.location.href == "https://violettides.github.io/"){
        document.getElementById("sun").classList.remove('visually-hidden');
        document.getElementById("moon").classList.add('visually-hidden');
        document.getElementById("meLight").classList.remove('visually-hidden');
-       document.getElementById("meDark").classList.add('visually-hidden');
+       document.getElementById("meDark").classList.add('visually-hidden'); 
+    }
+       
     
     document.body.classList.remove('dark');
     document.body.classList.add('light');
