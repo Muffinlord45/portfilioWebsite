73d677ed1f214218b9a40f25cdc44185002f8cfc
VioletTides <ross34anderson@gmail.com>
2022-10-01 22:56:42 -0400

    flash test 7

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,8 +10,6 @@ function switch_to_dark(){
     localStorage.setItem('theme', 'dark');
      console.log("dark");
        if(window.location.href == "https://violettides.github.io/"){
-           document.getElementById("sun").classList.add('visually-hidden');
-       document.getElementById("moon").classList.remove('visually-hidden');
        document.getElementById("meLight").classList.add('visually-hidden');
        document.getElementById("meDark").classList.remove('visually-hidden');
        }
@@ -24,8 +22,6 @@ function switch_to_light(){
     localStorage.setItem('theme', 'light');
     console.log("light");
     if(window.location.href == "https://violettides.github.io/"){
-       document.getElementById("sun").classList.remove('visually-hidden');
-       document.getElementById("moon").classList.add('visually-hidden');
        document.getElementById("meLight").classList.remove('visually-hidden');
        document.getElementById("meDark").classList.add('visually-hidden'); 
     }
