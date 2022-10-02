2c0490497c685969ee763c9b176fb5897b60cdae
VioletTides <ross34anderson@gmail.com>
2022-10-01 23:07:14 -0400

    flash test 10

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,17 @@
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    localStorage.setItem('theme', 'dark');
     switch_to_dark();
 }
 else{
+    localStorage.setItem('theme', 'light');
     switch_to_light();
 }
 
 
 function switch_to_dark(){
-    localStorage.setItem('theme', 'dark');
+    
      console.log("dark");
        if(window.location.href == "https://violettides.github.io/"){
-
        document.getElementById("meLight").classList.add('visually-hidden');
        document.getElementById("meDark").classList.remove('visually-hidden');
        }
@@ -20,10 +21,9 @@ function switch_to_dark(){
 }
 
 function switch_to_light(){
-    localStorage.setItem('theme', 'light');
+    
     console.log("light");
     if(window.location.href == "https://violettides.github.io/"){
-
        document.getElementById("meLight").classList.remove('visually-hidden');
        document.getElementById("meDark").classList.add('visually-hidden'); 
     }
